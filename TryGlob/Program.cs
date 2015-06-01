16e1f8e2d10356002d67891aa5690fa7e6b6735e
unknown <lianwei@lianwei-l1.fareast.corp.microsoft.com>
2015-06-01 08:58:42 +0800

    Add glob class

---
+++ TryGlob/Program.cs
@@ -0,0 +1,24 @@
+﻿using IronRuby.Builtins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryGlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            if (args.Length == 0) input = Console.ReadLine();
+            else input = args[0];
+            var results = Glob.GetMatches(input, 0);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
